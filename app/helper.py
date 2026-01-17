528e5efc0b2ac84e4017d7803bb2d0932bd2b87c
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-17 16:53:20 +0800

    get mrt codes to name mapping

---
+++ app/helper.py
@@ -0,0 +1,14 @@
+import json
+
+def get_mrt_lines():
+    station_names = {}
+    with open("mrt_all.json","r") as f:
+        data = json.loads(f.read())
+        for station in data.keys():
+            line_members = data[station]["lineMembers"]
+            for lines in line_members.keys():
+                for line in line_members[lines]:
+                    station_names[line["code"]] =  data[station]["name"]
+    return station_names
+
+print(get_mrt_lines())
