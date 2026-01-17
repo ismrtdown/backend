a81d4359b35eded8ef616e92e340ef75c9a2c23e
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-17 21:03:42 +0800

    timedata per 15 min interval

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+import math
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from flask import Flask, request
@@ -17,7 +18,28 @@ mrt_station_codes = stations.keys()
 
 app = Flask("sad")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+cache = []
+prev_record = datetime.now(tz=timezone.utc) - timedelta(days=1)
 
+async def get_graph_data():
+    try:
+        res = [] 
+        now = datetime.now(tz=timezone.utc)
+        yest = now - timedelta(days=1)
+        for i in range(96):
+            data = {
+                "time": yest + timedelta(minutes=15) * i,
+                "no.": 0
+            }
+            res.append(data)
+        response = supabase.table("down_reports").select("station_code", "created_at").gte("created_at",yest.isoformat()).lte("created_at", now.isoformat()).execute()
+        for report in response.data:
+            index = math.floor((datetime.fromisoformat(report["created_at"]) - yest).total_seconds() / 60 / 15)
+            res[index]["no."] += 1
+        return res
+    except APIError:
+        return None
+    
 def report_post(data):
     if not "station_code" in data:
         error = {
@@ -83,6 +105,20 @@ def report_get():
         print(e)
         return error, 500
 
+@app.route("/reportno", methods = ["GET"])
+async def reportno():
+    global cache, prev_record
+    now = datetime.now(tz = timezone.utc)
+    if (now - prev_record).total_seconds() / 60 > 5:
+        cache = await get_graph_data()
+        if cache is None:
+            error = {
+                "error": "server error"
+            }
+            return error, 500
+        prev_record = now
+    return cache, 200
+        
 @app.route("/report", methods = ["GET", "POST"])
 def report():
     print(request.method)
